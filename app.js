53ec75f6a705ca1a621d3d6d99af9b44507c37cc
Aditya24 <aadityabante@gmail.com>
2020-08-30 21:35:20 +0200

    Fetching API comments

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Selecting the DOM
 const textBtn = document.querySelector("#fetch-text");
 const employeeBtn = document.querySelector("#fetch-employee");
-const text = "text.txt";
+const commentsBtn = document.querySelector("#fetch-comments");
 const textOutput = document.querySelector("#text-output");
+const text = "text.txt";
+const url = "https://jsonplaceholder.typicode.com/comments";
 //Adding Event Listener
 textBtn.addEventListener("click", fetchText);
 employeeBtn.addEventListener("click", getEmployee);
+commentsBtn.addEventListener("click", getComments);
 //Creating Functions
-
+//Get Text
 function fetchText() {
   //Using ES5
   /*fetch("text.txt")
@@ -29,6 +32,7 @@ function fetchText() {
     //catching errors from promises
     .catch((err) => console.log(err));
 }
+//Get Users
 function getEmployee() {
   fetch("employee.json")
     .then((res) => res.json())
@@ -40,6 +44,27 @@ function getEmployee() {
        <li>ID:${employee.id} </li>
        <li>Name:${employee.name} </li>
        <li>Email:${employee.email} </li>
+       </ul>
+       `;
+      });
+      textOutput.innerHTML = showData;
+    })
+    .catch((err) => console.log(err));
+}
+//Get Comments
+
+function getComments() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      let showData = "<h3>Comments</h3>";
+      data.forEach((comment) => {
+        showData += `
+       <ul>
+       <li><strong>ID:</strong>${comment.postId} </li>
+       <li><strong>Name:</strong>${comment.name} </li>
+       <li><strong>Email:</strong>${comment.email} </li>
+       <li><strong>Comment:</strong>${comment.body} </li>
        </ul>
        `;
       });
