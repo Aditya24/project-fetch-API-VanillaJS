a46b18ca8a47579e0f4827fd2c6a2a991076eb90
Aditya24 <aadityabante@gmail.com>
2020-08-30 21:13:46 +0200

    Fetching data from JSON file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // Selecting the DOM
 const textBtn = document.querySelector("#fetch-text");
+const employeeBtn = document.querySelector("#fetch-employee");
 const text = "text.txt";
 const textOutput = document.querySelector("#text-output");
 //Adding Event Listener
 textBtn.addEventListener("click", fetchText);
-
+employeeBtn.addEventListener("click", getEmployee);
 //Creating Functions
 
 function fetchText() {
@@ -26,5 +27,23 @@ function fetchText() {
       textOutput.innerHTML = data;
     })
     //catching errors from promises
+    .catch((err) => console.log(err));
+}
+function getEmployee() {
+  fetch("employee.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let showData = "<h3>Employee</h3>";
+      data.forEach((employee) => {
+        showData += `
+       <ul>
+       <li>ID:${employee.id} </li>
+       <li>Name:${employee.name} </li>
+       <li>Email:${employee.email} </li>
+       </ul>
+       `;
+      });
+      textOutput.innerHTML = showData;
+    })
     .catch((err) => console.log(err));
 }
