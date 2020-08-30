be07ed17d6c2c3c02ae13b4ff5d7e4db8be1fe38
Aditya24 <aadityabante@gmail.com>
2020-08-30 22:52:11 +0200

    setting up POST request

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const textBtn = document.querySelector("#fetch-text");
 const employeeBtn = document.querySelector("#fetch-employee");
 const commentsBtn = document.querySelector("#fetch-comments");
 const textOutput = document.querySelector("#text-output");
+const addComments = document.querySelector("#addComments");
 const text = "text.txt";
 const url = "https://jsonplaceholder.typicode.com/comments";
 //Adding Event Listener
 textBtn.addEventListener("click", fetchText);
 employeeBtn.addEventListener("click", getEmployee);
 commentsBtn.addEventListener("click", getComments);
+addComments.addEventListener("submit", addComment);
 //Creating Functions
 //Get Text
 function fetchText() {
@@ -40,10 +42,10 @@ function getEmployee() {
       let showData = "<h3>Employee</h3>";
       data.forEach((employee) => {
         showData += `
-       <ul>
-       <li>ID:${employee.id} </li>
-       <li>Name:${employee.name} </li>
-       <li>Email:${employee.email} </li>
+       <ul class="list-group mb-4">
+       <li class="list-group-item">ID:${employee.id} </li>
+       <li class="list-group-item">Name:${employee.name} </li>
+       <li class="list-group-item">Email:${employee.email} </li>
        </ul>
        `;
       });
@@ -57,18 +59,39 @@ function getComments() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      let showData = "<h3>Comments</h3>";
+      let showData = '<h3 class="mb-4">Comments</h3>';
       data.forEach((comment) => {
         showData += `
-       <ul>
-       <li><strong>ID:</strong>${comment.postId} </li>
-       <li><strong>Name:</strong>${comment.name} </li>
-       <li><strong>Email:</strong>${comment.email} </li>
-       <li><strong>Comment:</strong>${comment.body} </li>
+       <div class="card-card-body mb-4">
+       <ul class="list-group mb-4>
+       <li class="list-group-item"><strong>ID:</strong>${comment.postId} </li>
+       <li class="list-group-item"><strong>Name:</strong>${comment.name} </li>
+       <li class="list-group-item"><strong>Email:</strong>${comment.email} </li>
+       <li class="list-group-item"><strong>Comment:</strong>${comment.body} </li>
        </ul>
+       </div>
        `;
       });
       textOutput.innerHTML = showData;
     })
     .catch((err) => console.log(err));
+}
+
+function addComment(e) {
+  //Because it's a form, we prevent it from submitting to a file
+  e.preventDefault();
+  //Getting the values of the title and the comment field
+  let name = document.querySelector("#name").value;
+  let body = document.querySelector("#body").value;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain */*",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ name: name, body: body }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
